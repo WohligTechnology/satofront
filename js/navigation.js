0004b992513633469f00c3d4830c11323c95c881
sohan1 <shn619@gmail.com>
2016-04-09 19:39:02 +0530

    changes for create match

--- js/navigation.js
+++ js/navigation.js
@@ -9,43 +9,32 @@ var navigationservice = angular.module('navigationservice', [])
 
 .factory('NavigationService', function() {
   var navigation = [{
-      name: "Home",
-      classis: "active",
-      anchor: "home",
-      subnav: [{
-        name: "Subnav1",
-        classis: "active",
-        link: "#/home"
-      }]
-    }, {
-      name: "Create Match",
-      classis: "active",
-      anchor: "creatematch",
-    }, {
-      name: "Edit Match",
-      classis: "active",
-      anchor: "editmatch",
-    }, {
-      name: "Edit Match",
-      classis: "active",
-      anchor: "editmatch",
-    }
-  ];
+    name: "Home",
+    classis: "active",
+    anchor: "home",
+  }, {
+    name: "Create Match",
+    classis: "active",
+    anchor: "creatematch",
+  }, {
+    name: "Edit Match",
+    classis: "active",
+    anchor: "editmatch",
+  }];
 
-return {
-  getnav: function() {
-    return navigation;
-  },
-  makeactive: function(menuname) {
-    for (var i = 0; i < navigation.length; i++) {
-      if (navigation[i].name == menuname) {
-        navigation[i].classis = "active";
-      } else {
-        navigation[i].classis = "";
+  return {
+    getnav: function() {
+      return navigation;
+    },
+    makeactive: function(menuname) {
+      for (var i = 0; i < navigation.length; i++) {
+        if (navigation[i].name == menuname) {
+          navigation[i].classis = "active";
+        } else {
+          navigation[i].classis = "";
+        }
       }
-    }
-    return menuname;
-  },
-
-};
+      return menuname;
+    },
+  };
 });
